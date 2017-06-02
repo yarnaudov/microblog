f0eb0374d27c9d40a1d7346b74d9589d1cada8ba
Yordan Arnaudov <y_arnaudov@yahoo.com>
2017-06-02 23:54:22 +0300

    remove sass from admin project

--- admin/src/templates/layout.php
+++ admin/src/templates/layout.php
@@ -3,6 +3,7 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Administration panel</title>
+        <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="/assets/css/styles.css">
     </head>
     <body>
